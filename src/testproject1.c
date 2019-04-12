c019c90aebd2456c7fa466adb4c7acc9583aecec
Administrator <Administrator@spdev.us-east1-b.c.smart-tracer-237303.internal>
2019-04-12 05:34:23 +0000

    V1.0

---
+++ src/testproject1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : testproject1.c
+ Author      : santosh
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World Demo!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
