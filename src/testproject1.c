3ee88830705a9e98b9b5ce78eb73487d769b5609
santoshkpadhi <49545415+santoshkpadhi@users.noreply.github.com>
2019-04-12 05:39:16 +0000

    v2.0

--- src/testproject1.c
+++ src/testproject1.c
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World Demo!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello World Demo --version2!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
